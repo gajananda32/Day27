6122e7613e5a572bdb2945377a097dc26e74cd1b
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-14 16:11:38 +0530

    learning problems

---
+++ BinarySerialization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    public class BinarySerialization
+    {
+        public void SerialiZation()
+        {
+            Demo sample = new Demo();
+            FileStream fileStream = new FileStream(@"C:\Users\Gajanan\source\repos\FileIODemo\FileIODemo\BinaryDemo.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, sample);
+        }
+
+        public void Deserlization()
+        {
+            FileStream fileStream = new FileStream(@"C:\Users\Gajanan\source\repos\FileIODemo\FileIODemo\BinaryDemo.txt", FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Demo deserilaizationSampleDemo = (Demo)formatter.Deserialize(fileStream);
+            Console.WriteLine("ApplicationName: " + deserilaizationSampleDemo.ApplicationName + "ApplicationID: " + deserilaizationSampleDemo.ApplicationID);
+        }
+        [Serializable]
+        public class Demo
+        {
+            public string ApplicationName { get; set; } = "BinarySerialzation";
+            public int ApplicationID { get; set; } = 1001;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using System.Data.SqlTypes;
+
+namespace FileIODemo;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string path = @"C:\Users\Gajanan\source\repos\FileIODemo\FileIODemo\Demo.txt";
+        //FileExitPath(path);
+        //ReadAll_Files_fromFile(path);
+        // ReadAllText(path);
+        //CoyFileToAnother(path);
+        // DeleteFile(path);
+        BinarySerialization b = new BinarySerialization();
+        //b.SerialiZation();
+        b.Deserlization();
+    }
+    public static void FileExitPath(string Path)
+    {
+        if (File.Exists(Path))
+        {
+            Console.WriteLine("File Exits");
+        }
+        else
+        {
+            Console.WriteLine("File not  Exits");
+        }
+    }
+    public static void ReadAll_Files_fromFile(string Path)
+    {
+        string[] lines;
+        lines = File.ReadAllLines(Path);
+        Console.WriteLine(lines[0]);
+        Console.WriteLine(lines[1]);
+        Console.WriteLine(lines[2]);
+    }
+    public static void ReadAllText(string Path)
+    {
+        string line;
+        line = File.ReadAllText(Path);
+        Console.WriteLine(line);
+    }
+    public static void DeleteFile(string Path)
+    {
+        string coypath= @"C:\Users\Gajanan\source\repos\FileIODemo\FileIODemo\Example.txt";
+        File.Delete(coypath);
+    }
+    public static void CoyFileToAnother(string Path)
+    {
+        string copypath = @"C:\Users\Gajanan\source\repos\FileIODemo\FileIODemo\Example.txt"; ;
+        File.Copy(Path, copypath);
+    }
+    public static void ReadFrom_StreamReader(string Path)
+    {
+        using (StreamReader sr=File.OpenText(Path))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
