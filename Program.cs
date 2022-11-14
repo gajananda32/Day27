c76dc55904f89599728caf24d1125c25e950bf8c
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-14 16:13:24 +0530

    Read or write using IO

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AddbUC13
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Gajanan\source\repos\AddbUC13\AddbUC13\File.txt";
+            ReadAllLinesFromFile(path);
+        }
+        public static void ReadAllLinesFromFile(string Path)
+        {
+            string lines;
+            lines = File.ReadAllText(Path);
+            Console.WriteLine(lines);
+        }
+    }
+}
